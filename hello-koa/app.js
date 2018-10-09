eba0ccede1508edf3cd2b915fa2ef384a4f795a9
Hopingocean <1258963839@qq.com>
2018-10-09 17:54:14 +0800

    koa

--- hello-koa/app.js
+++ hello-koa/app.js
@@ -20,7 +20,29 @@ app.use(async (ctx, next) => {
   ctx.response.body = '<h1>Hello, koa2!</h1>';
 })
 
+// 简写 ctx.url == ctx.request.url; ctx.type == ctx.response.type
+
+// 注意require('koa-router')返回的是函数
+const router = require('koa-router')();
+
+// log request URL
+app.use(async (ctx, next) => {
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+  await next();
+})
+
+// GET
+router.get('/hello/:name', async (ctx, next) => {
+  var name = ctx.params.name;
+  ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+router.get('/', async (ctx, next) => {
+  ctx.response.body = `<h1>Index</h1>`;
+});
+
+// add router middleware
+app.use(router.routes());
+
 app.listen(3000);
 console.log('app started at port 3000...');
-
-// 简写 ctx.url == ctx.request.url; ctx.type == ctx.response.type
---
+++ url-koa/app.js
@@ -0,0 +1,47 @@
+const Koa = require('koa');
+// 注意require('koa-router')返回的是函数
+const router = require('koa-router')();
+const bodyParser = require('koa-bodyparser');
+
+const app = new Koa();
+
+app.use(bodyParser);
+
+// log request URL
+app.use(async (ctx, next) => {
+  console.log(`Process ${ctx.request.method} ${ctx.request.url}...`);
+  await next();
+})
+
+// GET
+router.get('/hello/:name', async (ctx, next) => {
+  var name = ctx.params.name;
+  ctx.response.body = `<h1>Hello, ${name}!</h1>`;
+});
+
+// 处理POST请求
+router.get('/', async (ctx, next) => {
+  ctx.response.body = `<h1>Index</h1>
+    <form action="/login" method="post">
+      <p>username: <input name="username" value="Lee"></p>
+      <p>password: <input name="password" type="password"></p>
+      <p><input type="submit" value="Submit"></p>
+    </form>`;
+});
+
+router.post('/login', async (ctx, next) => {
+  var username = ctx.request.body.username || '';
+  var password = ctx.request.body.password || '';
+  if (username === 'Lee' && password === '123456') {
+    ctx.response.body = `<h1>Welcome, ${username}</h1>`;
+  } else {
+    ctx.response.body = `<h1>Login failed!</h1>
+    <p><a href="/">Try again</a></p>`;
+  }
+})
+
+// add router middleware
+app.use(router.routes());
+
+app.listen(3000);
+console.log('app started at port 3000...');
